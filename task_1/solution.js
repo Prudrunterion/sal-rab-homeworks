69207f20756b96a16df54f479f77ea38de3366a1
Prudrunterion <prudrunterion@gmail.com>
2021-10-09 19:13:38 +0300

    prepared tasks1

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,9 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
+    
+    let message = productName + ' за ' + productPrice + ' теперь в корзине!';
+    console.log(message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -23,6 +25,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = (oldValue) + 1;
+    console.log (newValue);
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -38,6 +42,9 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+    let newSum = (oldSum + difference);
+    let newSumText = newSum + ' ₽';
+    console.log(newSumText)
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
