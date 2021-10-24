07d98791b0d46641770bdf473293419d01cbd7b4
Prudrunterion <prudrunterion@gmail.com>
2021-10-24 19:35:45 +0300

    Tasks_final_1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,8 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+   
+    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
 
     let countOfGoods = goods.length;
 
