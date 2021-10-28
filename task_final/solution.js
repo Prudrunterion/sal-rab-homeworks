d51dce78bfa7711361604861a53989300fe6add3
Prudrunterion <prudrunterion@gmail.com>
2021-10-28 21:47:39 +0300

    final_test_4

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,15 +42,14 @@ function sendRequest(name, phone, address, goods, sum) {
  
     let countOfGoods = goods.length;
 
- 
 
     for (let i = 0; i < countOfGoods; i +=1) {
-        data.goods.push(goods[i].title);
-        data.goods.push(goods[i].count);
-            };
+        data.goods.push({title: goods[i].title, count: goods[i].count})
+    };
 
 
-    data.address = ['ул.' + ' ' + address.street, 'дом' + ' ' + address.house, address.entrance + ' ' + 'подъезд', address.floor + ' ' +'этаж', 'кв' + ' ' + address.flat];
+    data.order.address = (`ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`);
+    
     
     data.order.sum = sum;
 
